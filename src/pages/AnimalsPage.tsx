f5085ef33fd2e501ad68b17f45d272d9bf916fc2
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-08 01:11:59 +0200

    add: added pages for Users and Animals

--- src/pages/AnimalsPage.tsx
+++ src/pages/AnimalsPage.tsx
@@ -1,5 +1,32 @@
+import AnimalsTable from '@/components/AnimalsTable/AnimalsTable';
+import GenericTable from '@/components/BaseTable/BaseTable';
+
+const headers = [
+  {
+    name: 'ID',
+    className: 'w-[50px] hidden sm:flex items-center',
+  },
+  {
+    name: 'Name',
+  },
+  {
+    name: 'Gender',
+  },
+  {
+    name: 'Banned',
+    width: 'w-[100px]',
+  },
+  {
+    name: 'Actions',
+  },
+];
+
 const AnimalsPage = () => {
-  return <div>AnimalsPage</div>;
+  return (
+    <GenericTable headers={headers}>
+      <AnimalsTable />
+    </GenericTable>
+  );
 };
 
 export default AnimalsPage;
--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,5 +1,32 @@
+import GenericTable from '@/components/BaseTable/BaseTable';
+import UsersTable from '@/components/UsersTable/UsersTable';
+
+const headers = [
+  {
+    name: 'ID',
+    className: 'w-[50px] hidden sm:flex items-center',
+  },
+  {
+    name: 'Name',
+  },
+  {
+    name: 'Gender',
+  },
+  {
+    name: 'Banned?',
+    className: 'w-[100px]',
+  },
+  {
+    name: 'Actions',
+  },
+];
+
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  return (
+    <GenericTable headers={headers}>
+      <UsersTable />
+    </GenericTable>
+  );
 };
 
 export default UsersPage;
