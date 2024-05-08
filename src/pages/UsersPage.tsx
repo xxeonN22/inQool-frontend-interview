3409a7e9f8841c37fb49ac74f730b08257879afa
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-09 00:34:59 +0200

    add: added ids to headers, changed styling for headers, added logic for rendering table or table loader

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,31 +1,43 @@
 import GenericTable from '@/components/BaseTable/BaseTable';
 import UsersTable from '@/components/UsersTable/UsersTable';
+import UsersTableLoader from '@/components/UsersTable/UsersTableLoader';
+import useUsers from '@/hooks/useUsers';
 
 const headers = [
   {
-    name: 'ID',
-    className: 'w-[50px] hidden sm:flex items-center',
-  },
-  {
+    id: 2,
     name: 'Name',
+    className: 'px-0 sm:p-4 w-[40%]',
   },
   {
+    id: 3,
     name: 'Gender',
+    className: 'px-0 sm:p-4 w-[10%]',
   },
   {
-    name: 'Banned?',
-    className: 'w-[100px]',
+    id: 4,
+    name: 'Ban status',
+    className: 'px-0 sm:p-4 w-[25%] text-center',
   },
   {
+    id: 5,
     name: 'Actions',
+    className: 'px-0 sm:p-4 w-[25%] text-center',
   },
 ];
 
 const UsersPage = () => {
+  const { data, isLoading } = useUsers();
   return (
-    <GenericTable headers={headers}>
-      <UsersTable />
-    </GenericTable>
+    <>
+      {data && !isLoading ? (
+        <GenericTable headers={headers}>
+          <UsersTable users={data} />
+        </GenericTable>
+      ) : (
+        <UsersTableLoader />
+      )}
+    </>
   );
 };
 
