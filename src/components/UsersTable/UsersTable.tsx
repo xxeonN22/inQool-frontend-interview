59c6d32431204077631d1a77691eed6388e264f1
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-09 01:30:00 +0200

    add: added sorting to users table list

--- src/components/UsersTable/UsersTable.tsx
+++ src/components/UsersTable/UsersTable.tsx
@@ -1,10 +1,9 @@
 import { useContext, useMemo } from 'react';
 import { TableCell, TableRow } from '@/components/ui/table';
 import { FilterContext } from '@/components/BaseTable/BaseTable';
-import EditUser from '@/components/Forms/EditUser/EditUser';
 import { User } from '@/types/users';
-import DeleteUser from '../Forms/DeleteUser/DeleteUser';
-import { Button } from '../ui/button';
+import UserBanStatus from '@/components/UsersTable/UserBanStatus';
+import UserRowButtons from '@/components/UsersTable/UserRowButtons';
 
 interface UsersTableProps {
   users: User[];
@@ -13,41 +12,28 @@ interface UsersTableProps {
 const UsersTable = ({ users }: UsersTableProps) => {
   const filter = useContext(FilterContext);
 
-  const filteredUsers = useMemo(() => {
-    return users.filter((user) =>
+  const filteredAndSortedUsers = useMemo(() => {
+    const filtered = users.filter((user) =>
       user.name.toLowerCase().includes(filter.toLowerCase())
     );
+    const sorted = filtered.sort((a, b) => {
+      return a.id.localeCompare(b.id);
+    });
+
+    return sorted;
   }, [users, filter]);
 
   return (
     <>
-      {filteredUsers.map((user) => (
+      {filteredAndSortedUsers.map((user) => (
         <TableRow key={user.id}>
           <TableCell className="px-0 sm:p-4 w-[40%]">{user.name}</TableCell>
           <TableCell className="px-0 sm:p-4 w-[10%]">{user.gender}</TableCell>
           <TableCell className="px-0 sm:p-4 w-[25%] text-center">
-            {user.banned ? (
-              <div className="flex gap-2">
-                <Button size="sm">Remove ban</Button>
-              </div>
-            ) : (
-              <Button size="sm">Give ban</Button>
-            )}
+            <UserBanStatus user={user} />
           </TableCell>
           <TableCell className="px-0 sm:p-4 w-[25%] text-center">
-            <div className="flex flex-col gap-2 sm:flex-row sm:gap-8 justify-center items-center">
-              <EditUser
-                user={user}
-                trigger="Edit"
-                triggerClass="bg-primary text-primary-foreground px-2 py-1 rounded-sm hover:bg-accent"
-              />
-              <DeleteUser
-                userId={user.id}
-                userName={user.name}
-                trigger="Delete"
-                triggerClass="bg-destructive text-destructive-foreground px-2 py-1 rounded-sm hover:bg-red-700"
-              />
-            </div>
+            <UserRowButtons user={user} />
           </TableCell>
         </TableRow>
       ))}
