91930104a972425b34db94742bd6d7a73657dfcb
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-08 20:23:07 +0200

    add: added FieldValues, UseFormRegister, Path, register inside InputData component to handle form

--- src/components/InputData/InputData.tsx
+++ src/components/InputData/InputData.tsx
@@ -1,48 +1,45 @@
+import { FieldValues, UseFormRegister, Path } from 'react-hook-form';
+import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
-import { Label } from '../ui/label';
 
-interface InputDataProps {
+interface InputDataProps<T extends FieldValues> {
   className?: string;
   placeholder?: string;
-  name?: string;
   label?: string;
   id?: string;
   errorMessage?: string;
-  span?: string;
   autocomplete?: string;
   defaultValue?: string;
+  name: Path<T>;
+  register: UseFormRegister<T>;
 }
 
-const InputData = (props: InputDataProps) => {
-  const {
-    className,
-    placeholder,
-    label,
-    name,
-    id,
-    errorMessage,
-    span,
-    autocomplete,
-    defaultValue,
-  } = props;
-
+const InputData = <T extends FieldValues>({
+  className,
+  placeholder,
+  label,
+  id,
+  errorMessage,
+  autocomplete,
+  defaultValue,
+  name,
+  register,
+}: InputDataProps<T>) => {
   return (
-    <div className={span}>
+    <div>
       {errorMessage && (
         <span className="text-destructive inline-block mb-1 text-sm">
           {errorMessage}
         </span>
       )}
-      <Label htmlFor={name} className="text-right">
-        {label}
-      </Label>
+      <Label htmlFor={id}>{label}</Label>
       <Input
         className={`focus:border-accent focus-visible:ring-accent focus-visible:ring-offset-0 ${className}`}
+        defaultValue={defaultValue}
         placeholder={placeholder}
-        name={name}
+        {...register(name)}
         id={id}
         autoComplete={autocomplete}
-        defaultValue={defaultValue}
       />
     </div>
   );
