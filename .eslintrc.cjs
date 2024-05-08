6951b1b80966a1858ffbacf2f31b7d4a93710f99
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-09 00:03:32 +0200

    add: added no-uesless-fragment rule inside eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -33,6 +33,7 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 0,
     'import/extensions': 0,
+    'react/jsx-no-useless-fragment': 0,
     'react/require-default-props': 0,
     'react/jsx-props-no-spreading': 0,
     'react/function-component-definition': [
