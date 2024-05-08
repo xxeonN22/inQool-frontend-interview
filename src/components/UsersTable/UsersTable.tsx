ade5a60b1f39068c84f995dc8f8b8082a96d7ff9
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-09 00:34:06 +0200

    add: added users prop to UsersTable component, fixed width of table columns, added styling for edit and delete buttons

--- src/components/UsersTable/UsersTable.tsx
+++ src/components/UsersTable/UsersTable.tsx
@@ -1,37 +1,56 @@
-import React, { useContext, useMemo } from 'react';
+import { useContext, useMemo } from 'react';
 import { TableCell, TableRow } from '@/components/ui/table';
-import { Badge } from '@/components/ui/badge';
 import { FilterContext } from '@/components/BaseTable/BaseTable';
-import users from '@/components/UsersTable/mockData';
 import EditUser from '@/components/Forms/EditUser/EditUser';
+import { User } from '@/types/users';
+import DeleteUser from '../Forms/DeleteUser/DeleteUser';
+import { Button } from '../ui/button';
 
-const UsersTable = () => {
+interface UsersTableProps {
+  users: User[];
+}
+
+const UsersTable = ({ users }: UsersTableProps) => {
   const filter = useContext(FilterContext);
 
   const filteredUsers = useMemo(() => {
     return users.filter((user) =>
       user.name.toLowerCase().includes(filter.toLowerCase())
     );
-  }, [filter]);
+  }, [users, filter]);
 
   return (
-    <TableRow>
-      {filteredUsers.map((user) => {
-        return (
-          <React.Fragment key={user.id}>
-            <TableCell className="hidden sm:block">{user.id}</TableCell>
-            <TableCell className="text-center">{user.name}</TableCell>
-            <TableCell className="text-center">{user.gender}</TableCell>
-            <TableCell className="text-center">
-              {user.banned && <Badge>{user.banned ? 'BANNED' : ''}</Badge>}
-            </TableCell>
-            <TableCell className="flex flex-col sm:flex-row justify-center items-center gap-3">
-              <EditUser user={user} trigger="Edit" />
-            </TableCell>
-          </React.Fragment>
-        );
-      })}
-    </TableRow>
+    <>
+      {filteredUsers.map((user) => (
+        <TableRow key={user.id}>
+          <TableCell className="px-0 sm:p-4 w-[40%]">{user.name}</TableCell>
+          <TableCell className="px-0 sm:p-4 w-[10%]">{user.gender}</TableCell>
+          <TableCell className="px-0 sm:p-4 w-[25%] text-center">
+            {user.banned ? (
+              <div className="flex gap-2">
+                <Button size="sm">Remove ban</Button>
+              </div>
+            ) : (
+              <Button size="sm">Give ban</Button>
+            )}
+          </TableCell>
+          <TableCell className="px-0 sm:p-4 w-[25%] text-center">
+            <div className="flex flex-col gap-2 sm:flex-row sm:gap-8 justify-center items-center">
+              <EditUser
+                user={user}
+                trigger="Edit"
+                triggerClass="bg-primary text-primary-foreground px-2 py-1 rounded-sm hover:bg-accent"
+              />
+              <DeleteUser
+                userName={user.name}
+                trigger="Delete"
+                triggerClass="bg-destructive text-destructive-foreground px-2 py-1 rounded-sm hover:bg-red-700"
+              />
+            </div>
+          </TableCell>
+        </TableRow>
+      ))}
+    </>
   );
 };
 
