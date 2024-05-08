f633f813fc39979af3d8261e84375db92054fc6b
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-08 20:18:19 +0200

    add: added open and onOpenChange props to Modal component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,11 +4,19 @@ interface ModalProps {
   children: React.ReactNode;
   trigger: React.ReactNode | string;
   className?: string;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
 }
 
-const Modal = ({ children, trigger, className }: ModalProps) => {
+const Modal = ({
+  children,
+  trigger,
+  className,
+  open,
+  onOpenChange,
+}: ModalProps) => {
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogTrigger>{trigger}</DialogTrigger>
       <DialogContent className={className}>{children}</DialogContent>
     </Dialog>
