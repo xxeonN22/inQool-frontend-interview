a100df0baa47a9ce56a2b7d42d39b0a5cf194a50
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-09 23:06:36 +0200

    add: added NotFoundPage content and styling

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <>
+      <h3 className="mb-8 text-center">
+        You were not supposed to end up here, error ocurred, sorry.
+      </h3>
+      <Button className="px-7 py-3 mx-auto">
+        <Link to="/">Back to the home page</Link>
+      </Button>
+    </>
+  );
 };
 
 export default NotFoundPage;
