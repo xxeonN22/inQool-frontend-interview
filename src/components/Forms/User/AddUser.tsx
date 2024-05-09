948c4ecfdd74fcdc2f2d8f18655419531b89662a
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-09 12:36:26 +0200

    add: addd AddUser, DeleteUser, EditUser compoenents

---
+++ src/components/Forms/User/AddUser.tsx
@@ -0,0 +1,36 @@
+import Modal from '@/components/Modal/Modal';
+import UserFormContent from '@/components/Forms/UserFormContent';
+import useAddUser from '@/hooks/useAddUser';
+import AddNewEntityHeader from '@/components/Forms/AddNewEntityHeader';
+import AddNewEntityFooter from '@/components/Forms/AddNewEntityFooter';
+
+interface AddNewUserProps {
+  trigger: React.ReactNode | string;
+  triggerClass?: string;
+}
+
+const AddUser = ({ trigger, triggerClass }: AddNewUserProps) => {
+  const { open, setOpen, handleSubmit, onSubmit, register, control, errors } =
+    useAddUser();
+
+  return (
+    <Modal
+      trigger={trigger}
+      open={open}
+      onOpenChange={setOpen}
+      triggerClass={triggerClass}
+    >
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <AddNewEntityHeader entityName="user" />
+        <UserFormContent
+          register={register}
+          control={control}
+          errors={errors}
+        />
+        <AddNewEntityFooter entityName="user" />
+      </form>
+    </Modal>
+  );
+};
+
+export default AddUser;
---
+++ src/components/Forms/User/EditUser.tsx
@@ -0,0 +1,46 @@
+import Modal from '@/components/Modal/Modal';
+import UserFormContent from '@/components/Forms/UserFormContent';
+import EntityEditFormFooter from '@/components/Forms/EntityEditFormFooter';
+import EntityEditFormHeader from '@/components/Forms/EntityEditFormHeader';
+import { User } from '@/types/users';
+import useEditUser from '@/hooks/useEditUser';
+
+interface EditUserProps {
+  user: User;
+  trigger: React.ReactNode | string;
+  triggerClass?: string;
+}
+
+const EditUser = ({ user, trigger, triggerClass }: EditUserProps) => {
+  const {
+    register,
+    handleSubmit,
+    onSubmit,
+    control,
+    errors,
+    open,
+    setOpen,
+    isSubmitting,
+  } = useEditUser(user);
+
+  return (
+    <Modal
+      trigger={trigger}
+      open={open}
+      onOpenChange={setOpen}
+      triggerClass={triggerClass}
+    >
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <EntityEditFormHeader entityName={user.name} />
+        <UserFormContent
+          register={register}
+          control={control}
+          errors={errors}
+        />
+        <EntityEditFormFooter isSubmitting={isSubmitting} />
+      </form>
+    </Modal>
+  );
+};
+
+export default EditUser;
