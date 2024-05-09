a3a9aa3c4009db1b59f4da971f4a3fa8d3b95c3b
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-09 16:36:58 +0200

    fix: removed isSubmitting from EntityEDitFormFooter

--- src/components/Forms/EntityEditFormFooter.tsx
+++ src/components/Forms/EntityEditFormFooter.tsx
@@ -1,16 +1,10 @@
 import { Button } from '@/components/ui/button';
 import { DialogFooter } from '@/components/ui/dialog';
 
-interface EntityEditFormFooterProps {
-  isSubmitting: boolean;
-}
-
-const EntityEditFormFooter = ({ isSubmitting }: EntityEditFormFooterProps) => {
+const EntityEditFormFooter = () => {
   return (
     <DialogFooter>
-      <Button type="submit" disabled={isSubmitting}>
-        Confirm edit
-      </Button>
+      <Button type="submit">Confirm edit</Button>
     </DialogFooter>
   );
 };
