10d28ad774e1d651a186d355fcac143072362a57
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-09 17:18:16 +0200

    add: added custom message for animals zod schema validation

--- src/validationSchemas/animal.ts
+++ src/validationSchemas/animal.ts
@@ -1,9 +1,13 @@
 import { z } from 'zod';
 
 export const formSchema = z.object({
-  name: z.string().min(3),
+  name: z
+    .string()
+    .min(3, { message: 'Animal name must be at least 3 characters long.' }),
   type: z.enum(['cat', 'dog', 'other']),
-  age: z.string().regex(/^\d+$/),
+  age: z.string().regex(/^\d+$/, {
+    message: 'Age can be only number',
+  }),
 });
 
 export type FormValues = z.infer<typeof formSchema>;
