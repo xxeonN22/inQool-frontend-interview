a914890f874c0f1f0755a8518e44e998a1c49c66
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-09 16:32:48 +0200

    add: added RootLayout to main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import router from '@/router';
 import '@/index.css';
+import RootLayout from './layouts/RootLayout';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <RootLayout>
+        <RouterProvider router={router} />
+      </RootLayout>
     </QueryClientProvider>
   </React.StrictMode>
 );
