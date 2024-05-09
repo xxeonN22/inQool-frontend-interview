498ae63e67694447a64880793a25a740eb78cb69
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-09 16:46:57 +0200

    add: added back toast to useBanUser hook

--- src/hooks/useBanUser.ts
+++ src/hooks/useBanUser.ts
@@ -1,8 +1,10 @@
 import { User, UserForm } from '@/types/users';
+import { useToast } from '@/components/ui/use-toast';
 import { useUserEdit } from '@/hooks/useUsers';
 
 const useBanUser = (user: User) => {
   const { mutate: editUser } = useUserEdit(user.id);
+  const { toast } = useToast();
 
   const handleEdit = async () => {
     const payload: UserForm = {
@@ -10,7 +12,22 @@ const useBanUser = (user: User) => {
       gender: user.gender,
       banned: !user.banned,
     };
-    editUser(payload);
+    editUser(payload, {
+      onSuccess: () => {
+        toast({
+          title: user.banned
+            ? `Successfully removed ban for user ${user.name}`
+            : `${user.name} was successfully banned`,
+          variant: 'primary',
+        });
+      },
+      onError: () => {
+        toast({
+          title: `There was error while setting new ban status for user, try again`,
+          variant: 'destructive',
+        });
+      },
+    });
   };
 
   return handleEdit;
