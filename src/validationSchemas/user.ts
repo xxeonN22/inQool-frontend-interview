9d51e84ea3b88012b2a7c1bc80f4b1b344eb50ce
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-09 16:32:21 +0200

    add: added custom error message to formSchema of user

--- src/validationSchemas/user.ts
+++ src/validationSchemas/user.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod';
 
 export const formSchema = z.object({
-  name: z.string().min(3),
+  name: z
+    .string()
+    .min(3, { message: 'User name must be at least 3 characters long.' }),
   gender: z.enum(['male', 'female', 'other']),
 });
 
