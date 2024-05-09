b1999b3a1f74588321a24f13674230d703f6744a
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-09 23:29:09 +0200

    fix: moved fetching data for users from UsersPage to TableDataUsers component, fixed UsersTableLoader - removed headers

--- src/components/Users/TableDataUsers.tsx
+++ src/components/Users/TableDataUsers.tsx
@@ -3,16 +3,17 @@ import { TableCell, TableRow } from '@/components/ui/table';
 import { FilterContext } from '@/components/BaseTable/BaseTable';
 import UserBanStatus from '@/components/Users/UserBanStatus';
 import UserRowButtons from '@/components/Users/UserRowButtons';
-import { User } from '@/types/users';
+import useUsers from '@/hooks/useUsers';
+import UsersTableLoader from './UsersTableLoader';
 
-interface TableDataUsersProps {
-  users: User[];
-}
-
-const TableDataUsers = ({ users }: TableDataUsersProps) => {
+const TableDataUsers = () => {
+  const { data: users, isLoading } = useUsers();
   const filter = useContext(FilterContext);
 
   const filteredAndSortedUsers = useMemo(() => {
+    if (!users) {
+      return [];
+    }
     const filtered = users.filter((user) =>
       user.name.toLowerCase().includes(filter.toLowerCase())
     );
@@ -25,18 +26,22 @@ const TableDataUsers = ({ users }: TableDataUsersProps) => {
 
   return (
     <>
-      {filteredAndSortedUsers.map((user) => (
-        <TableRow key={user.id}>
-          <TableCell className="px-0 sm:p-4 w-[40%]">{user.name}</TableCell>
-          <TableCell className="px-0 sm:p-4 w-[10%]">{user.gender}</TableCell>
-          <TableCell className="px-0 sm:p-4 w-[25%] text-center">
-            <UserBanStatus user={user} />
-          </TableCell>
-          <TableCell className="px-0 sm:p-4 w-[25%] text-center">
-            <UserRowButtons user={user} />
-          </TableCell>
-        </TableRow>
-      ))}
+      {isLoading ? (
+        <UsersTableLoader />
+      ) : (
+        filteredAndSortedUsers.map((user) => (
+          <TableRow key={user.id}>
+            <TableCell className="px-0 sm:p-4 w-[40%]">{user.name}</TableCell>
+            <TableCell className="px-0 sm:p-4 w-[10%]">{user.gender}</TableCell>
+            <TableCell className="px-0 sm:p-4 w-[25%] text-center">
+              <UserBanStatus user={user} />
+            </TableCell>
+            <TableCell className="px-0 sm:p-4 w-[25%] text-center">
+              <UserRowButtons user={user} />
+            </TableCell>
+          </TableRow>
+        ))
+      )}
     </>
   );
 };
--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,7 +1,5 @@
 import BaseTable from '@/components/BaseTable/BaseTable';
 import TableDataUsers from '@/components/Users/TableDataUsers';
-import UsersTableLoader from '@/components/Users/UsersTableLoader';
-import useUsers from '@/hooks/useUsers';
 import AddUser from '@/components/Forms/User/AddUser';
 
 const headers = [
@@ -28,25 +26,20 @@ const headers = [
 ];
 
 const UsersPage = () => {
-  const { data, isLoading } = useUsers();
   return (
     <>
-      {data && !isLoading ? (
-        <BaseTable
-          headers={headers}
-          searchPlaceholder="Search by name..."
-          addButton={
-            <AddUser
-              trigger="Add new user"
-              triggerClass="bg-primary text-primary-foreground rounded-md px-4 py-2"
-            />
-          }
-        >
-          <TableDataUsers users={data} />
-        </BaseTable>
-      ) : (
-        <UsersTableLoader />
-      )}
+      <BaseTable
+        headers={headers}
+        searchPlaceholder="Search by name..."
+        addButton={
+          <AddUser
+            trigger="Add new user"
+            triggerClass="bg-primary text-primary-foreground rounded-md px-4 py-2"
+          />
+        }
+      >
+        <TableDataUsers />
+      </BaseTable>
     </>
   );
 };
