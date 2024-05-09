0462bf9ea50c07a5a2423ec77454fd872d8cfaf8
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-09 12:35:57 +0200

    add: added AddAnimal, DeleteAnimal, EditAnimal components

---
+++ src/components/Forms/Animal/AddAnimal.tsx
@@ -0,0 +1,36 @@
+import Modal from '@/components/Modal/Modal';
+import AddNewEntityHeader from '@/components/Forms/AddNewEntityHeader';
+import AddNewEntityFooter from '@/components/Forms/AddNewEntityFooter';
+import AnimalFormContent from '@/components/Forms/AnimalFormContent';
+import useAddAnimal from '@/hooks/useAddAnimal';
+
+interface AddAnimalProps {
+  trigger: React.ReactNode | string;
+  triggerClass?: string;
+}
+
+const AddAnimal = ({ trigger, triggerClass }: AddAnimalProps) => {
+  const { open, setOpen, handleSubmit, control, onSubmit, register, errors } =
+    useAddAnimal();
+
+  return (
+    <Modal
+      trigger={trigger}
+      open={open}
+      onOpenChange={setOpen}
+      triggerClass={triggerClass}
+    >
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <AddNewEntityHeader entityName="animal" />
+        <AnimalFormContent
+          register={register}
+          control={control}
+          errors={errors}
+        />
+        <AddNewEntityFooter entityName="animal" />
+      </form>
+    </Modal>
+  );
+};
+
+export default AddAnimal;
---
+++ src/components/Forms/Animal/EditAnimal.tsx
@@ -0,0 +1,45 @@
+import Modal from '@/components/Modal/Modal';
+import AnimalFormContent from '@/components/Forms/AnimalFormContent';
+import EntityEditFormHeader from '@/components/Forms/EntityEditFormHeader';
+import EntityEditFormFooter from '@/components/Forms/EntityEditFormFooter';
+import { Animal } from '@/types/animals';
+import useEditAnimal from '@/hooks/useEditAnimal';
+
+interface EditAnimalProps {
+  animal: Animal;
+  trigger: React.ReactNode | string;
+  triggerClass?: string;
+}
+
+const EditAnimal = ({ animal, trigger, triggerClass }: EditAnimalProps) => {
+  const {
+    register,
+    handleSubmit,
+    onSubmit,
+    control,
+    errors,
+    open,
+    setOpen,
+    isSubmitting,
+  } = useEditAnimal(animal);
+  return (
+    <Modal
+      trigger={trigger}
+      open={open}
+      onOpenChange={setOpen}
+      triggerClass={triggerClass}
+    >
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <EntityEditFormHeader entityName={animal.name} />
+        <AnimalFormContent
+          register={register}
+          control={control}
+          errors={errors}
+        />
+        <EntityEditFormFooter isSubmitting={isSubmitting} />
+      </form>
+    </Modal>
+  );
+};
+
+export default EditAnimal;
