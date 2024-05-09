e15d9d7f715a940c22dfed40f807acecfcd14887
Samuel Tuka <xtuka@fi.muni.cz>
2024-05-09 22:57:29 +0200

    fix: fixed ToolsSection component - added grird-cols-1 for small screen

--- src/components/HomePage/ToolsSection.tsx
+++ src/components/HomePage/ToolsSection.tsx
@@ -7,7 +7,7 @@ const ToolsSection = () => {
       <h3 className="mb-5 text-center">
         Tools that were used while making this web application
       </h3>
-      <ul className="grid grid-cols-2 md:grid-cols-4 text-center gap-3">
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 text-center gap-3">
         {tools.map((tool) => (
           <UsedTool key={tool.id} name={tool.name} />
         ))}
